d21122488937f938a2d01cd4af044786fe00ebfe
Paulo Vieira <paulovieira@gmail.com>
2015-01-11 12:23:11 +0000

    applied js beautifier

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var Backbone = require("backbone"),
-	       _ = require('underscore'),
-	       Q = require('q'),
-	      pg = require('pg').native;
+    _ = require('underscore'),
+    Q = require('q'),
+    pg = require('pg').native;
 
 var changeCaseKeys = require('change-case-keys');
 
@@ -16,48 +16,52 @@ The execute method will simply do this:
 Note: the .then method returns a new promise, which we retun to the caller
 */
 Backbone.Collection.prototype.execute = function(options) {
-	var promise, 
-		options = options || {},
-		collection = this,
-		method = options.reset ? "reset" : "set";
+    var promise,
+        options = options || {},
+        collection = this,
+        method = options.reset ? "reset" : "set";
 
-	// change the case of the keys (or the objects in the result.rows array); pass false to disable;
-	// should be one of: "camelize" (default), "underscored" or "dasherize" (underscore.string methods)
-	options.changeCase = _.result(options, "changeCase");
-	if(options.changeCase==="undefined"){ options.changeCase = _.result(collection, "changeCase"); }
+    // change the case of the keys (or the objects in the result.rows array); pass false to disable;
+    // should be one of: "camelize" (default), "underscored" or "dasherize" (underscore.string methods)
+    options.changeCase = _.result(options, "changeCase");
+    if (options.changeCase === "undefined") {
+        options.changeCase = _.result(collection, "changeCase");
+    }
 
-	if(options.changeCase !== false){  
-		options.changeCase = _.isString(options.changeCase) ? options.changeCase : "camelize";
-	}
+    if (options.changeCase !== false) {
+        options.changeCase = _.isString(options.changeCase) ? options.changeCase : "camelize";
+    }
 
-	promise = this
-			.sync(this, options)
-			.then(
-				function(resp){
-debugger;
-					// allow the .parse method to be overriden directly at the options level
-					// (this allows for customized parse function for the different usages - 
-					// read/create/update/delete; note that the collection-level .parse method
-					// will still be called at .set()/reset() (the default implementation is a noop )
-					if(_.isFunction(options.parse)){ resp = options.parse(resp, options); }
+    promise = this
+        .sync(this, options)
+        .then(
+            function(resp) {
+                debugger;
+                // allow the .parse method to be overriden directly at the options level
+                // (this allows for customized parse function for the different usages - 
+                // read/create/update/delete; note that the collection-level .parse method
+                // will still be called at .set()/reset() (the default implementation is a noop )
+                if (_.isFunction(options.parse)) {
+                    resp = options.parse(resp, options);
+                }
 
-					collection[method](resp, options);
-					collection.trigger('sync', collection, resp, options);
+                collection[method](resp, options);
+                collection.trigger('sync', collection, resp, options);
 
-					return resp;
-				}, 
-				function(err) {
-debugger;
-					collection.trigger('error', collection, err, options);
-					throw err;
-	    		}
-			);
+                return resp;
+            },
+            function(err) {
+                debugger;
+                collection.trigger('error', collection, err, options);
+                throw err;
+            }
+    );
 
-	return promise;
+    return promise;
 };
 
 Backbone.Collection.prototype.sync = function() {
-	return Backbone.syncPostgres.apply(this, arguments);
+    return Backbone.syncPostgres.apply(this, arguments);
 },
 
 
@@ -68,64 +72,69 @@ update the collection with the output (either through .set() or .reset())
 ****/
 
 Backbone.syncPostgres = function(entity, options) {
-debugger;
-	options = options || {};
+    //debugger;
+    options = options || {};
 
-	var propertyError = function(property) {
-		throw new Error('A "' + property + '" property or function must be specified');
-	};
+    var propertyError = function(property) {
+        throw new Error('A "' + property + '" property or function must be specified');
+    };
 
-	// Ensure that we have a connection string/object in the options or in the entity itself
-	// (connection can be either given as a string/obj or as a function that returns a string/obj)
-	options.connection =   _.result(options, "connection") 
-						|| _.result(entity, 'connection') 
-						|| propertyError('connection');
+    // Ensure that we have a connection string/object in the options or in the entity itself
+    // (connection can be either given as a string/obj or as a function that returns a string/obj)
+    options.connection = _.result(options, "connection") || _.result(entity, 'connection') || propertyError('connection');
 
-	// same for the query object
-	options.query =    _.result(options, 'query') 
-					|| _.result(entity, 'query') 
-					|| propertyError('query');
+    // same for the query object
+    options.query = _.result(options, 'query') || _.result(entity, 'query') || propertyError('query');
 
-	// if the query accepts only 1 parameter, allow it to be given directly in query.arguments
-	// (instead of using an array with that single parameter)
-	var command = options.query.command;
-	if(command.indexOf("$1") >= 0 && command.indexOf("$2") < 0){
-		if(!_.isArray(options.query.arguments) && !_.isUndefined(options.query.arguments)){
-			options.query.arguments = [options.query.arguments]
-		}
-	}
+    // if the query accepts only 1 parameter, allow it to be given directly in query.arguments
+    // (instead of using an array with that single parameter)
+    var command = options.query.command;
+    if (command.indexOf("$1") >= 0 && command.indexOf("$2") < 0) {
+        if (!_.isArray(options.query.arguments) && !_.isUndefined(options.query.arguments)) {
+            options.query.arguments = [options.query.arguments];
+        }
 
-	var deferred = Q.defer();
-	options.promise = deferred.promise;
+    }
 
-	pg.connect(options.connection, function(err, client, done) {
-		if(err) {  deferred.reject(err); return;  }
+    var deferred = Q.defer();
+    options.promise = deferred.promise;
 
-		// currently we are using simple parameterized queries; later we should switch to Prepared Statements:
-		// more info: https://github.com/brianc/node-postgres/wiki/Prepared-Statements
-		client.query(options.query.command, options.query.arguments, function(err, result) {
+    pg.connect(options.connection, function(err, client, done) {
+        if (err) {
+            deferred.reject(err);
+            return;
+        }
 
-			done();
+        // currently we are using simple parameterized queries; later we should switch to Prepared Statements:
+        // more info: https://github.com/brianc/node-postgres/wiki/Prepared-Statements
+        client.query(options.query.command, options.query.arguments, function(err, result) {
 
-			if(err) {  deferred.reject(err); return;  }
+            done();
 
-			// change the case of the keys
-			if(options.changeCase){ 
-				changeCaseKeys(result.rows, options.changeCase);
-			}
-			deferred.resolve(result.rows);
+            if (err) {
+                deferred.reject(err);
+                return;
+            }
 
-			if(options.disconnect){ pg.end(); }
-		});
-	});
+            // change the case of the keys
+            if (options.changeCase) {
+                changeCaseKeys(result.rows, options.changeCase);
+            }
+            deferred.resolve(result.rows);
 
-	entity.trigger('request', entity, options.promise, options);
-	return deferred.promise;
+            if (options.disconnect) {
+                pg.end();
+            }
+        });
+    });
+
+    entity.trigger('request', entity, options.promise, options);
+    return deferred.promise;
 };
 
 
 Backbone.Collection.prototype.disconnect = function() {
-	pg.end();	
+    pg.end();
 };
 
 module.exports = Backbone;
